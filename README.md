ed82986bc420849588d9c2d271c83647ee6de538
Gurunandan R. Bhat <gbhat@pobox.com>
2013-05-14 18:36:33 +0530

    Initial DZilla template commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+Kelp::Module::ValidateTiny - Blah blah blah
+
+# SYNOPSIS
+
+    use Kelp::Module::ValidateTiny;
+
+# DESCRIPTION
+
+Kelp::Module::ValidateTiny is
+
+# AUTHOR
+
+Gurunandan R. Bhat <gbhat@pobox.com>
+
+# COPYRIGHT
+
+Copyright 2013- Gurunandan R. Bhat
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/Kelp/Module/ValidateTiny.pm
@@ -0,0 +1,39 @@
+package Kelp::Module::ValidateTiny;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Kelp::Module::ValidateTiny - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use Kelp::Module::ValidateTiny;
+
+=head1 DESCRIPTION
+
+Kelp::Module::ValidateTiny is
+
+=head1 AUTHOR
+
+Gurunandan R. Bhat E<lt>gbhat@pobox.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2013- Gurunandan R. Bhat
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
